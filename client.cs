1cb3052226cc59b05fd77c424d1c570b87854e92
Weitz <73654789+We1tz@users.noreply.github.com>
2023-08-02 12:25:23 +0300

    update files
    the first)
    
    add console chat (server >> client & client >> server)
    add first commands

---
+++ client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    public void Start()
+    {
+        try
+        {
+            // Подключение к серверу
+            string serverIP = "127.0.0.1"; // Используйте IP-адрес сервера
+            int port = 8888; // Используйте номер порта, указанный на сервере
+            TcpClient client = new TcpClient(serverIP, port);
+            Console.WriteLine("Подключение к серверу выполнено.");
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                // Отправляем сообщение серверу
+                Console.Write("Клиент: ");
+                string clientMessage = Console.ReadLine();
+                SendMessage(stream, clientMessage);
+
+                if (clientMessage.ToLower() == "выход")
+                {
+                    break;
+                }
+
+                // Получаем ответ от сервера
+                string serverMessage = ReadMessage(stream);
+                Console.WriteLine("Сервер: " + serverMessage);
+            }
+
+            // Закрываем соединение с сервером
+            client.Close();
+            Console.WriteLine("Отключение от сервера.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+
+    private void SendMessage(NetworkStream stream, string message)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        stream.Write(buffer, 0, buffer.Length);
+    }
+
+    private string ReadMessage(NetworkStream stream)
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    }
+}
---
+++ server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    public void Start()
+    {
+        try
+        {
+            // Устанавливаем сервер и начинаем прослушивать входящие соединения
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1"); // Используйте желаемый IP-адрес сервера
+            int port = 8888; // Используйте желаемый номер порта
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключения клиента...");
+
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Клиент подключен.");
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                // Получаем сообщение от клиента
+                string clientMessage = ReadMessage(stream);
+
+                if (clientMessage.ToLower() == "выход")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Клиент: " + clientMessage);
+
+                // Отправляем ответ клиенту
+                Console.Write("Сервер: ");
+                string serverMessage = Console.ReadLine();
+                SendMessage(stream, serverMessage);
+            }
+
+            // Закрываем соединение с клиентом
+            client.Close();
+            Console.WriteLine("Клиент отключен.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+
+    private string ReadMessage(NetworkStream stream)
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    }
+
+    private void SendMessage(NetworkStream stream, string message)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(message);
+        stream.Write(buffer, 0, buffer.Length);
+    }
+}
