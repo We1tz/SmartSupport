da58708205580a575aadf54c37a9856897123887
Weitz <73654789+We1tz@users.noreply.github.com>
2023-08-07 11:50:13 +0300

    fix
    add command "screen"

--- server.cs
+++ server.cs
@@ -2,17 +2,29 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Diagnostics;
 
 class Server
 {
+    private string screenshotsPath = "C:\\Users\\105(2)\\source\\repos\\SmartVoiceAdm\\SmartVoiceAdm"; 
+
+    public Server()
+    {
+        if (!Directory.Exists(screenshotsPath))
+        {
+            Directory.CreateDirectory(screenshotsPath);
+        }
+    }
+
     public void Start()
     {
         try
         {
-            // Устанавливаем сервер и начинаем прослушивать входящие соединения
-            IPAddress ipAddress = IPAddress.Parse("127.0.0.1"); // IP 
-            int port = 8888; 
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
             TcpListener listener = new TcpListener(ipAddress, port);
             listener.Start();
             Console.WriteLine("Сервер запущен. Ожидание подключения клиента...");
@@ -24,23 +36,26 @@ class Server
 
             while (true)
             {
-                // Получаем сообщение от клиента
                 string clientMessage = ReadMessage(stream);
 
                 if (clientMessage.ToLower() == "выход")
                 {
                     break;
                 }
+                else if (clientMessage.ToLower() == "screen")
+                {
+                    byte[] screenshotBytes = ReceiveScreenshot(stream, ReadInt32(stream)); // Чтение длины массива
+                    ShowScreenshot(screenshotBytes);
+                    continue;
+                }
 
                 Console.WriteLine("Клиент: " + clientMessage);
 
-                // Отправляем ответ клиенту
                 Console.Write("Сервер: ");
                 string serverMessage = Console.ReadLine();
                 SendMessage(stream, serverMessage);
             }
 
-            // Закрываем соединение с клиентом
             client.Close();
             Console.WriteLine("Клиент отключен.");
         }
@@ -61,5 +76,40 @@ class Server
     {
         byte[] buffer = Encoding.UTF8.GetBytes(message);
         stream.Write(buffer, 0, buffer.Length);
+    }
+
+    private int ReadInt32(NetworkStream stream)
+    {
+        byte[] intBytes = new byte[4];
+        stream.Read(intBytes, 0, 4);
+        return BitConverter.ToInt32(intBytes, 0);
+    }
+
+    private byte[] ReceiveScreenshot(NetworkStream stream, int length)
+    {
+        byte[] buffer = new byte[length];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        return buffer;
+    }
+
+    private void ShowScreenshot(byte[] screenshotBytes)
+    {
+        try
+        {
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            string screenshotPath = Path.Combine(screenshotsPath, $"Screenshot_{timestamp}.png");
+
+            using (MemoryStream memoryStream = new MemoryStream(screenshotBytes))
+            {
+                Image screenshot = Image.FromStream(memoryStream);
+                screenshot.Save(screenshotPath);
+                Console.WriteLine($"Скриншот сохранен: {screenshotPath}");
+                Process.Start(screenshotPath); 
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при сохранении скриншота: " + ex.Message);
+        }
     }
 }
