c2e8402d5bbae5e9d57bb536a2803091e184c650
Raphael <120366621+Raphael-Santi@users.noreply.github.com>
2023-10-31 16:50:15 +0300

    Animate.js for CUBIC_View0.1

---
+++ CUBIC/animate.js
@@ -0,0 +1,13 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+  let options = { threshold: [0.1] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.animation');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
