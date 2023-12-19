aac47559e11738b3f0730c396011bff4fb9c4cce
Raphael <120366621+Raphael-Santi@users.noreply.github.com>
2023-12-19 21:40:01 +0300

    Delete react-with-electron-test01 directory

--- react-with-electron-test01/src/api/config.js
+++
@@ -1,7 +0,0 @@
-export const apiHost = '';
-
-export const apiLogin = 'login';
-export const apiUserStatus = 'users/status';
-export const apiList = 'lists';
-export const apiBasicData = 'lists/basics';
-
