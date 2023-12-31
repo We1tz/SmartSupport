34d8cb79f5315fcb1df73e4e96bb6eb3a8902e62
Raf <120366621+rafl999@users.noreply.github.com>
2023-12-31 16:38:58 +0300

    commit git	modified:   .gitignore commit git	modified:   README.md commit git	deleted:    Sign Up Frame -2.png commit git	deleted:    Sign Up Frame .png commit git	deleted:    cubic_admin-dashboard.png commit git	new file:   electron/main.js commit git	new file:   package-lock.json commit git	new file:   package.json commit git	new file:   public/favicon.ico commit git	new file:   public/index.html commit git	new file:   public/logo192.png commit git	new file:   public/logo512.png commit git	new file:   public/manifest.json commit git	new file:   public/robots.txt commit git	new file:   src/App.css commit git	new file:   src/App.js commit git	new file:   src/App.test.js commit git	new file:   src/index.css commit git	new file:   src/index.js commit git	new file:   src/logo.svg commit git	new file:   src/reportWebVitals.js commit git	new file:   src/setupTests.js

---
+++ electron/main.js
@@ -0,0 +1,54 @@
+// Модули для управления приложением и создания окна
+const {app, BrowserWindow} = require('electron')
+const path = require ('path');
+const url = require ('url');
+//require('./ipcmain');
+
+function createWindow () {
+  // Создаем окно браузера.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // и загрузить index.html приложения.
+  //mainWindow.loadFile('index.html')
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl);
+  // mainWindow.loadURL('http://localhost:3000');
+
+  // Отображаем средства разработчика.
+  // mainWindow.webContents.openDevTools()
+}
+
+// Этот метод вызывается когда приложение инициализируется
+// и будет готово для создания окон.
+// Некоторые API могут использоваться только после возникновения этого события.
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    // На MacOS обычно пересоздают окно в приложении,
+    // после того, как на иконку в доке нажали и других открытых окон нету.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Выйти когда все окна закрыты
+app.on('window-all-closed', function () {
+  // Для приложений и строки меню в macOS является обычным делом оставаться
+  // активными до тех пор, пока пользователь не выйдет окончательно используя Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// В этом файле вы можете включить остальную часть основного процесса вашего приложения
+//  Вы также можете поместить их в отдельные файлы и подключить через require.
