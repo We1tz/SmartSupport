42c083e0050f46ca25352ec6d7e059b76c9d635b
Raf <120366621+rafl999@users.noreply.github.com>
2024-01-10 16:28:04 +0300

    commit git	deleted:    src/App.css commit git	modified:   src/App.js commit git	deleted:    src/App.test.js commit git	deleted:    src/index.css commit git	modified:   src/index.js commit git	deleted:    src/logo.svg commit git	deleted:    src/reportWebVitals.js commit git	deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 //const electron = window.require('electron');
 //const ipcRenderer  = electron.ipcRenderer;
